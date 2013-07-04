bfd8dce0991bb0c40ea8ab84db00ef9ad5392b72
Pablo Vallejo <pabloninety@gmail.com>
2013-07-04 08:00:41 -0500

    Start category model

--- blogengine/models.py
+++ blogengine/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+# Category Model
+class Category( models.Model ):
+    title = models.CharField( max_length = 200 )
+    slug = models.SlugField( max_length = 40, unique = True )
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __unicode__( self ):
+        return self.title
+
+    def get_absolute_url( self ):
+        return '/categories/{}'.format( self.slug )
+
+
 # Post model
 class Post( models.Model ):
     title = models.CharField( max_length = 200 )
@@ -18,3 +35,7 @@ class Post( models.Model ):
         return '/%s/%s/%s' % ( self.pub_date.year, self.pub_date.month, self.slug )
 
 
+# Category To Post
+class CategoryToPost( models.Model ):
+    post = models.ForeignKey( Post )
+    category = models.ForeignKey( Category )
